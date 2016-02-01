5641b676850382cd0eebc7f758b37e74fec1294a
Masen3429 <lueckemasen@gmail.com>
2016-02-01 16:40:52 -0500

    Create SysUpdater.sh

---
+++ bin/SysUpdater.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+trap '' 2
+clear
+ping -c 5 www.google.com
+while true
+do 
+clear
+echo "Masen's --- Basic --- System --- Updater"
+echo "-----------------------------------------------------"
+echo
+echo "Are you sure you want to refresh system repository's?"
+echo
+echo "Enter y to update system repository's y: "
+echo "Enter n to quit SysUpdater script n: "
+read input
+case "$input" in
+  y) sudo apt-get update ;;
+  n) exit ;;
+esac
+echo "Are you sure you want to upgrade the system repository's?"
+echo "Enter y to upgrade system repository's y: "
+echo "Enter n to quit SysUpdater script n: "
+read input
+case "$input" in
+  y) sudo apt-get dist-upgrade ;;
+  n) exit ;;
+esac
+done
